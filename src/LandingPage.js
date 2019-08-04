980e7c39f491c901950e992954d58c3906ee20e3
Zac Smith <zrsmith75@users.noreply.github.com>
2019-08-04 10:33:33 -0600

    create LandingPage

---
+++ src/LandingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <h2>LandingPage</h2>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { LandingPage } from "./LandingPage";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Protected Routes</h1>
+      <LandingPage />
     </div>
   );
 }
